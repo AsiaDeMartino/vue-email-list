9a4afd1d9a14312fac9dd942e4dd3e76c958adf3
asia_demartino@libero.it <asia_demartino@libero.it>
2022-03-07 01:08:48 +0100

    generate email con API

--- myscript.js
+++ myscript.js
@@ -3,6 +3,17 @@ const app = new Vue({
     data: {
         mail: [],
     },
+  created() {
+    for (let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (response) => {
+            
+            (this.mail).push(response.data.response);
+            console.log(this.mail);
+            console.log(response.data.response);
 
+        });
+    }
+}
 
 })
